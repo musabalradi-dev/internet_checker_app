fdb3fd04b08ff31c9ec5cca37da0fea26f35d297
Musab Alradi <78719015+musabalradi-dev@users.noreply.github.com>
2025-04-19 16:44:51 +0200

    first commit

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
         BlocProvider<ConnectivityBloc>(create: (context) => ConnectivityBloc()),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: const SettingsScreen(),
       ),
     );
